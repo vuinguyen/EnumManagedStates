cd574516327098f7b84c5e88c02522c11d56295e
Vui Nguyen <vui_engineer@yahoo.com>
2020-11-15 16:24:19 -0700

    first commit

---
+++ EnumManagedStates/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  EnumManagedStates
+//
+//  Created by Vui Nguyen on 11/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+
+            Button(action: {
+                //
+            }) {
+                Text("Check Connectivity")
+            }
+
+            Text("Hello, world!")
+                .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EnumManagedStates/EnumManagedStatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnumManagedStatesApp.swift
+//  EnumManagedStates
+//
+//  Created by Vui Nguyen on 11/15/20.
+//
+
+import SwiftUI
+
+@main
+struct EnumManagedStatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
